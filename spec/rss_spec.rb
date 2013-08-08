93f4ecf5b6099aa597880b2ed061875f08254c72
Tom Maher <tmaher@pw0n.me>
2013-08-07 17:36:32 -0700

    trivial read & write back test

--- spec/rss_spec.rb
+++ spec/rss_spec.rb
@@ -3,9 +3,24 @@ require 'spec_helper'
 describe SecretService do
 
   it "can create a session" do
+    SecretService.new.class.should_not be nil
   end
 
-  it "has collections" do
+  it "has a default collection" do
+    SecretService.new.collection.should_not be nil
+  end
+
+  it "can create and read back objects" do
+    name = SecureRandom.hex(8).to_s
+    secret = SecureRandom.hex(8).to_s
+    ss = SecretService.new
+    ss.collection.create_item("rspec_test_#{name}", secret).should_not be nil
+    ss.collection.get_secret("rspec_test_#{name}").should eq secret
+  end
+  
+  it "can create locked objects" do
+    ss = SecretService.new
+    coll = ss.collection
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'securerandom'
 
 $: << File.dirname(__FILE__) + '/../lib'
 
@@ -7,7 +8,9 @@ require 'secret_service'
 
 RSpec.configure do |config|
   config.color_enabled = true
-
+  config.order = 'random'
+  config.seed = SecureRandom.random_number(2 ** 24)
+  
   # Use color not only in STDOUT but also in pagers and files
   config.tty = true
 
